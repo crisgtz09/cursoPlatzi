7e4ad69c62a9127c83499e023b7198ae4477e82b
Brandon Montoya <brandon.montoya@bancaprepa.com>
2022-08-26 17:25:25 -0600

    .

--- API-prueba/src/main/java/com/caprepa/API/prueba/services/CancelReservationService.java
+++ API-prueba/src/main/java/com/caprepa/API/prueba/services/CancelReservationService.java
@@ -2,7 +2,7 @@ package com.caprepa.API.prueba.services;
 
 import com.caprepa.API.prueba.exceptions.BookingException;
 
-public interface CancelReservationService {
+public interface CancelReservationService implements Cancel{
 
     public String deleteReservation(String locator) throws BookingException;
 
--- API-prueba/src/main/java/com/caprepa/API/prueba/services/impl/CancelReservationServiceImpl.java
+++ API-prueba/src/main/java/com/caprepa/API/prueba/services/impl/CancelReservationServiceImpl.java
@@ -2,8 +2,11 @@ package com.caprepa.API.prueba.services.impl;
 
 import com.caprepa.API.prueba.exceptions.BookingException;
 import com.caprepa.API.prueba.services.CancelReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CancelReservationServiceImpl implements CancelReservationService {
+
+    @Autowired
 
     public String deleteReservation(String locator) throws BookingException {
 
